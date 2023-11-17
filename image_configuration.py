fdc60aaceb1e47beac74f92ed1c8b0cc03f03892
Rafael Menezes Villar <raphaelmenezesvill@gmail.com>
2023-11-17 08:21:07 -0300

    adicionei uma função 'extract_pixel' para extrair os pixel das imagens que serao usadas na rede neural, proximos passos: criar a rede em si por onde os dados serao passados.

--- image_configuration.py
+++ image_configuration.py
@@ -1,7 +1,6 @@
 from PIL import Image
 
 img = Image.open("numbers.png")
-
 def picar_imagem(img, quantidade, gap_horizontal, gap_vertical):
     nomes = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"]
     qtd = 0
@@ -38,4 +37,23 @@ def picar_imagem(img, quantidade, gap_horizontal, gap_vertical):
             if ini_y in [66, 265]:
                 ini_y += 1
                 fim_y += 1
-#picar_imagem(img, 160, 7, 5)
+
+def extract_pixels(img:Image):
+    box = img.getbbox()
+    ini_x = box[0]
+    ini_y = box[1]
+    fim_x = box[2]
+    fim_y = box[3]
+    pixel_list = []
+    
+    while ini_y != fim_y:
+        
+        if ini_x >= fim_x:
+            ini_x  = 0
+            ini_y += 1
+        if ini_y < fim_y:
+            pixel_list.append(img.getpixel((ini_x, ini_y))[0:3])
+        ini_x += 1
+    return pixel_list
+
+print(extract_pixels(img))
